84df66cc1d11a64eb6afd5bbaed0cfc2d4f89bfc
Kamran Khatti <kamran.khatti@ureed.com>
2022-03-03 19:28:25 +0500

    removed mock class object

--- src/app/modules/tutor/containers/tutor-dashboard/tutor-dashboard.component.ts
+++ src/app/modules/tutor/containers/tutor-dashboard/tutor-dashboard.component.ts
@@ -3,9 +3,8 @@ import { map } from 'rxjs/operators';
 import { combineLatest, Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
-import { IClassroom } from '@models';
 import * as fromCore from '@metutor/core/state';
-import { ClassroomType, insightRange, WEEK_DAYS } from '@metutor/config';
+import { insightRange, WEEK_DAYS } from '@metutor/config';
 
 @Component({
   selector: 'metutors-tutor-dashboard',
@@ -15,42 +14,7 @@ import { ClassroomType, insightRange, WEEK_DAYS } from '@metutor/config';
 export class TutorDashboardComponent implements OnInit {
   range = '7days';
   insightRange = insightRange;
-  rate = 4;
-
   view$: Observable<{ loading: boolean; data: any }>;
-
-  classroom: IClassroom = {
-    id: 1,
-    startDate: '2022-12-12',
-    expectedEndDate: '2022-12-30',
-    name: 'Python for Data Science and Machine Learning Boo …',
-    type: ClassroomType.one,
-    listDays: ['Fri', 'Sat', 'Sun'],
-    completedClasses: 10,
-    totalHours: 30,
-    startETime: new Date(),
-    endETime: new Date(),
-    remainingClasses: 10,
-    progress: 30,
-    enrolledStudents: [
-      {
-        id: 1,
-        avatar: 'https://logo.clearbit.com/tarjama.com',
-      },
-      {
-        id: 2,
-        avatar: 'https://logo.clearbit.com/noon.ae',
-      },
-      {
-        id: 3,
-        avatar: 'https://logo.clearbit.com/tamatem.co',
-      },
-      {
-        id: 4,
-        avatar: '',
-      },
-    ],
-  };
 
   constructor(private _store: Store<any>) {}
 
