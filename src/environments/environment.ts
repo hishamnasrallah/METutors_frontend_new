beb66eb724a6ee49a4c7f4b27512192ccc7ea4d3
Eslam Shapsough <e.shapsough@sit-mena.com>
2022-03-05 18:41:41 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-
   production: false,
   API_URL: 'https://test.zaptatechnologies.com/public/api/',
   // API_URL: 'http://160.153.255.132:8000/', // https://demo-metutors.herokuapp.com/
