963bb41781c124014f69577f4e48d16c20b9b6cd
Kamran Khatti <kamran.khatti@ureed.com>
2022-03-11 16:24:52 +0500

    added route param in router link

--- src/app/modules/tutor/containers/tutor-classes/tutor-classes.component.ts
+++ src/app/modules/tutor/containers/tutor-classes/tutor-classes.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { IUser } from '@metutor/core/models';
-import * as fromCore from '@metutor/core/state';
-import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { IUser } from '@metutor/core/models';
+import { Component, OnInit } from '@angular/core';
+
+import * as fromCore from '@metutor/core/state';
+import * as fromRouterStore from '@metutor/state';
+import { Params } from '@angular/router';
 
 @Component({
   selector: 'metutors-tutor-classes',
@@ -11,10 +14,12 @@ import { Observable } from 'rxjs';
 })
 export class TutorClassesComponent implements OnInit {
   user$: Observable<IUser | null>;
+  routeParams$: Observable<Params>;
 
   constructor(private _store: Store<any>) {}
 
   ngOnInit(): void {
     this.user$ = this._store.select(fromCore.selectUser);
+    this.routeParams$ = this._store.select(fromRouterStore.selectRouteParams);
   }
 }
