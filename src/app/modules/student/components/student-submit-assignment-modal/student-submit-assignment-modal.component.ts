11013711b86cad664bcf3ff29111a49d80753d97
Kamran Khatti <kamran.khatti@ureed.com>
2022-03-30 22:42:55 +0500

    added ngrx for submit assignment

--- src/app/modules/student/components/student-submit-assignment-modal/student-submit-assignment-modal.component.ts
+++ src/app/modules/student/components/student-submit-assignment-modal/student-submit-assignment-modal.component.ts
@@ -1,4 +1,17 @@
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { tap } from 'rxjs/operators';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import {
+  FormGroup,
+  Validators,
+  FormBuilder,
+  AbstractControl,
+} from '@angular/forms';
+
+import { generalConstants } from '@config';
+import * as fromCore from '@metutor/core/state';
 
 @Component({
   selector: 'metutors-student-submit-assignment-modal',
@@ -10,7 +23,44 @@ export class StudentSubmitAssignmentModalComponent implements OnInit {
 
   @Output() closeModal: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor() {}
+  form: FormGroup;
 
-  ngOnInit(): void {}
+  uploadedFiles$: Observable<any>;
+  fileUploadProgress$: Observable<any>;
+  isSubmittingAssignment$: Observable<boolean>;
+  uploadComplete = generalConstants.uploadComplete;
+
+  constructor(private _fb: FormBuilder, private _store: Store<any>) {}
+
+  onSubmit(): void {}
+
+  removeFile(id: number): void {
+    this._store.dispatch(fromCore.deleteUploadedFile({ id }));
+  }
+
+  onFileChange(event: any): void {
+    if (event.target && event.target.files && event.target.files.length) {
+      const file = [...event.target.files];
+      this._store.dispatch(fromCore.uploadFile({ file }));
+    }
+  }
+
+  get files(): AbstractControl | null {
+    return this.form?.get('files');
+  }
+
+  ngOnInit(): void {
+    this.fileUploadProgress$ = this._store.select(
+      fromCore.selectFileUploadingProgress
+    );
+
+    this.uploadedFiles$ = this._store
+      .select(fromCore.selectUploadedFiles)
+      .pipe(tap((files) => this.files?.setValue(files)));
+
+    this.form = this._fb.group({
+      files: [null, Validators.required],
+      description: [null, [Validators.required, Validators.minLength(10)]],
+    });
+  }
 }
