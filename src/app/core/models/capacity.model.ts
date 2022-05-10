ce62e0fe9425bea156178320a2f9e4badd3d695d
Ahmed <ahmed.hassan@ureed.com>
2022-05-10 18:04:51 +0200

    Workforce Capacity

---
+++ src/app/core/models/capacity.model.ts
@@ -0,0 +1,37 @@
+import { ISubject } from './subject.model';
+
+export class ICapacity {
+  id!: number;
+  subject: ISubject;
+  totalBookings: number;
+  totalRevenue: number;
+  hiredTutors: number;
+  capacity: number;
+  availableTutors: number;
+
+  constructor(createDefault = false, capacity: any = null) {
+    if (createDefault) {
+      this.id = 0;
+      this.subject = new ISubject(false);
+      this.totalBookings = 0;
+      this.totalRevenue = 0;
+      this.hiredTutors = 0;
+      this.capacity = 0;
+      this.availableTutors = 0;
+    }
+
+    if (capacity) {
+      this.id = capacity.id;
+      this.subject = new ISubject(false, capacity?.subject);
+      this.totalBookings = capacity?.total_bookings;
+      this.totalRevenue = capacity?.total_revenue;
+      this.hiredTutors = capacity?.hired_tutors;
+      this.availableTutors = capacity?.available_tutors;
+      this.capacity = capacity?.capacity;
+    }
+  }
+}
+
+export interface ICapacityFilters {
+  name: string;
+}
--- src/app/core/services/admin.service.ts
+++ src/app/core/services/admin.service.ts
@@ -1,8 +1,9 @@
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from 'src/environments/environment';
+import { ICapacity, ISubject } from '../models';
 
 @Injectable({
   providedIn: 'root',
@@ -30,5 +31,23 @@ export class AdminService {
       `${this.baseUrl}admin/reject-document/${id}`,
       {}
     );
+  }
+
+  loadWorkforceCapacity(): Observable<any> {
+    return this.http
+      .get<any>(`${this.baseUrl}admin/workforce-capacity`)
+      .pipe(
+        map((response) =>
+          response?.subjects.map(
+            (capacity: any) => new ICapacity(false, capacity)
+          )
+        )
+      );
+  }
+
+  loadCourseBookingList(id: number): Observable<any> {
+    return this.http
+      .get<any>(`${this.baseUrl}admin/subject/${id}/bookings`)
+      .pipe(map((response) => new ISubject(false, response?.subject)));
   }
 }
