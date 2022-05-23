8aa9f37fa893c8a82cd1274691f459cdb0fead35
Kamran Khatti <kamran.khatti@ureed.com>
2022-05-23 23:02:08 +0500

    modified popup

--- src/app/modules/admin/components/admin-student-total-booking-modal/admin-student-total-booking-modal.component.ts
+++ src/app/modules/admin/components/admin-student-total-booking-modal/admin-student-total-booking-modal.component.ts
@@ -8,12 +8,25 @@ import { environment } from '@environment';
 })
 export class AdminStudentTotalBookingModalComponent implements OnInit {
   @Input() type: string;
-  @Input() detail: any;
   @Input() loading = false;
   @Input() showModal = false;
 
+  @Input() set detail(value: any) {
+    if (this.type === 'subject') {
+      this.heading = value.subject?.name;
+      this.data = {
+        totalBookings: value.subject?.totalBookings,
+        bookings: [...value.subject?.courses],
+      };
+    } else {
+      this.data = value;
+    }
+  }
+
   @Output() closeModal: EventEmitter<void> = new EventEmitter<void>();
 
+  data: any;
+  heading = 'Booking Details';
   imageUrl = environment.imageURL;
 
   constructor() {}
