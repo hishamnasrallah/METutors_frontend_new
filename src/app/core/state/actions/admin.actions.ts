208dc6358137a3475cb18a3c1c249a271330c10a
Kamran Khatti <kamran.khatti@ureed.com>
2022-06-12 19:20:04 +0500

    fixed required param issue

--- src/app/core/state/actions/admin.actions.ts
+++ src/app/core/state/actions/admin.actions.ts
@@ -303,7 +303,7 @@ export const loadAdminStudentAssignmentSummaryFailure = createAction(
 
 export const loadAdminViewFeedback = createAction(
   '[Admin] Load Admin View Feedback',
-  props<{ id: number; teacherId: number }>()
+  props<{ id?: number; teacherId?: number }>()
 );
 
 export const loadAdminViewFeedbackSuccess = createAction(
