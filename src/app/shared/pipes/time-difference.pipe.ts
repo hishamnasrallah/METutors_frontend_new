149cb4feac5187da96c854cd00264f530ac33162
Kamran Khatti <kamran.khatti@ureed.com>
2022-10-14 22:05:03 +0500

    fixed time diff pipe

--- src/app/shared/pipes/time-difference.pipe.ts
+++ src/app/shared/pipes/time-difference.pipe.ts
@@ -10,7 +10,13 @@ export class TimeDifferencePipe implements PipeTransform {
   // todo make it dynamic to accept any time date and return different in days, hours and minutes
   transform(start: string, end: string): number {
     if (start && end) {
-      const duration = moment.duration(moment(end).diff(moment(start)));
+      const startTimeFormat = moment(start).format('hh:mm');
+      const endTimeFormat = moment(end).format('hh:mm');
+
+      const endTime = moment(endTimeFormat, 'HH:mm a');
+      const startTime = moment(startTimeFormat, 'HH:mm a');
+
+      const duration = moment.duration(endTime.diff(startTime));
 
       return duration.asHours();
     }
