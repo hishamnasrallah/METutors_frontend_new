c732918e0c169c995b913633e19c9125ad120e38
Kamran Khatti <kamran.khatti@ureed.com>
2022-10-26 21:49:36 +0500

    Added certificate with download option

--- src/app/modules/student/containers/student-view-certificate/student-view-certificate.component.ts
+++ src/app/modules/student/containers/student-view-certificate/student-view-certificate.component.ts
@@ -1,6 +1,8 @@
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import domtoimage from 'dom-to-image';
 
 import { IUser } from '@models';
 import * as fromRoot from '@metutor/state';
@@ -12,10 +14,23 @@ import * as fromCore from '@metutor/core/state';
   styleUrls: ['./student-view-certificate.component.scss'],
 })
 export class StudentViewCertificateComponent implements OnInit {
+  @ViewChild('certificate', { static: false }) certificate: ElementRef;
+
   layout$: any;
   user$: Observable<IUser | null>;
 
   constructor(private _store: Store<any>) {}
+
+  printCertificate(): void {}
+
+  downloadCertificate(): void {
+    domtoimage.toJpeg(this.certificate.nativeElement).then(function (dataUrl) {
+      const link = document.createElement('a');
+      link.download = 'certificate.jpeg';
+      link.href = dataUrl;
+      link.click();
+    });
+  }
 
   ngOnInit(): void {
     this.layout$ = this._store.select(fromRoot.selectLayout);
